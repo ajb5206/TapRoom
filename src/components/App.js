fa2bc56a4fc3f7d1adfc8556ea00450439c0d249
Alex Bertotto <ajb5206@gmail.com>
2022-01-13 13:51:01 -0800

    add header.js with text and imported to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
   return (
     <React.Fragment>
-      <h1>Tap Room</h1>
+      <Header />
       <h3>Keg name</h3>
       <h5>Keg brand</h5>
       <h5>Keg price</h5>
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+	return (
+		<React.Fragment>
+			<h1>Alex's Tap Room</h1>
+			<h4>Portland's Premier BierHaus</h4>
+		</React.Fragment>
+	);
+}
+
+export default Header;
