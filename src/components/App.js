b874b626dcf38be32193f588d626f8c8dd72cc25
Alex Bertotto <ajb5206@gmail.com>
2022-01-13 13:40:09 -0800

    create initial app.js with placeholder text

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Tap Room</h1>
+      <h3>Keg name</h3>
+      <h5>Keg brand</h5>
+      <h5>Keg price</h5>
+      <h5>Keg alcoholContent</h5>
+    </React.Fragment>
+  );
+}
+
+export default App;
